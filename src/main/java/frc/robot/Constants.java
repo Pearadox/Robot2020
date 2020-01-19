0a6f5197124185f33fcd00d4b9826a27046eb1ac
Bryan Li <bryanyuezhouli@gmail.com>
2020-01-19 16:29:01 -0600

    style and robot skeleton

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants.  This class should not be used for any other purpose.  All constants should be
@@ -16,4 +18,21 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final class DrivetrainConstants {
+    public static final double DRIVE_BASE_WIDTH = 0.0d; // Meters per second
+    public static final double WHEEL_DIAMETER = 0.0d; // Meters per second
+    
+    public static final double DISTANCE_PER_REVOLUTION = WHEEL_DIAMETER * Math.PI;
+    public static final int PULSES_PER_REVOLUTION = 0;
+    public static final double DISTANCE_PER_PULSE = DISTANCE_PER_REVOLUTION / PULSES_PER_REVOLUTION;
+    public static final DifferentialDriveKinematics KINEMATICS = 
+        new DifferentialDriveKinematics(DRIVE_BASE_WIDTH);
+
+    public static final double MAX_SPEED = 0.0d; // Meters per second
+    public static final double MAX_ACCELERATION = 0.0d; // Meters per second^2
+  }
+
+  public static final class RamseteConstants {
+    public static final double kS = 0.0d; // Volts
+  }
 }
