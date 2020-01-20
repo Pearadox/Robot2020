cf82438c30d79d4982ad3ca513a5a53542873216
CelvinM <lxcwmxl@gmail.com>
2020-01-20 14:09:31 -0600

    Drive Base
    Created the basic drivetrain with an odemetry and encoders as well as updated the CAN IDs.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.util.Units;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -19,20 +20,37 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
  */
 public final class Constants {
   public static final class DrivetrainConstants {
-    public static final double DRIVE_BASE_WIDTH = 0.0d; // Meters per second
-    public static final double WHEEL_DIAMETER = 0.0d; // Meters per second
+    public static final int MASTER_RIGHT_MOTOR = 0;
+    public static final int SLAVE_RIGHT_MOTOR1 = 1;
+    public static final int SLAVE_RIGHT_MOTOR2 = 2;
+    
+    public static final int MASTER_LEFT_MOTOR = 3;
+    public static final int SLAVE_LEFT_MOTOR1 = 4;
+    public static final int SLAVE_LEFT_MOTOR2 = 5;
+
+    public static final double DRIVE_BASE_WIDTH = 0.0d; // Meters 
+    public static final double WHEEL_DIAMETER = Units.inchesToMeters(6.0d); // Meters 
     
     public static final double DISTANCE_PER_REVOLUTION = WHEEL_DIAMETER * Math.PI;
     public static final int PULSES_PER_REVOLUTION = 0;
     public static final double DISTANCE_PER_PULSE = DISTANCE_PER_REVOLUTION / PULSES_PER_REVOLUTION;
+    public static final double SECONDS_PER_MINUTE = 60.0d;
+
     public static final DifferentialDriveKinematics KINEMATICS = 
         new DifferentialDriveKinematics(DRIVE_BASE_WIDTH);
+
+    public static final double THROTTLE_DEADBAND = 0.1d;
+    public static final double TWIST_DEADBAND = 0.1d;
+    public static final double MAX_OUTPUT = 1.0d;
 
     public static final double MAX_SPEED = 0.0d; // Meters per second
     public static final double MAX_ACCELERATION = 0.0d; // Meters per second^2
   }
 
   public static final class RamseteConstants {
+    public static final double B = 0.0d;
+    public static final double ZETA = 0.0d;
+
     public static final double kS = 0.0d; // Volts
   }
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,24 +3,150 @@
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+/*-----------------------------
+
+-----------------------------------------------*/
 
 package frc.robot.subsystems;
 
 import static frc.robot.Constants.DrivetrainConstants.*;
 
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
 
 public class Drivetrain extends SubsystemBase {
   /**
    * Creates a new Drivetrain.
-   */
-  public Drivetrain() {
+   */ 
+   
+  private final CANSparkMax leftMasterMotor;
+  private final CANSparkMax leftSlaveMotor1;
+  private final CANSparkMax leftSlaveMotor2;
+  private final CANSparkMax rightMasterMotor;
+  private final CANSparkMax rightSlaveMotor1;
+  private final CANSparkMax rightSlaveMotor2;
 
+  private final CANEncoder leftMasterEncoder;
+  private final CANEncoder leftSlaveEncoder1;
+  private final CANEncoder leftSlaveEncoder2;
+  private final CANEncoder rightMasterEncoder;
+  private final CANEncoder rightSlaveEncoder1;
+  private final CANEncoder rightSlaveEncoder2;
+
+  private final AHRS gyro;
+
+  private final DifferentialDriveOdometry odometry;
+
+  
+  
+  public Drivetrain() {
+    leftMasterMotor = new CANSparkMax(MASTER_LEFT_MOTOR, MotorType.kBrushless);
+    leftSlaveMotor1 = new CANSparkMax(SLAVE_LEFT_MOTOR1, MotorType.kBrushless);
+    leftSlaveMotor2 = new CANSparkMax(SLAVE_LEFT_MOTOR2, MotorType.kBrushless);
+
+    rightMasterMotor = new CANSparkMax(MASTER_RIGHT_MOTOR, MotorType.kBrushless);
+    rightSlaveMotor1 = new CANSparkMax(SLAVE_RIGHT_MOTOR1, MotorType.kBrushless);
+    rightSlaveMotor2 = new CANSparkMax(SLAVE_RIGHT_MOTOR2, MotorType.kBrushless);
+    
+    leftSlaveMotor1.follow(leftMasterMotor);
+    leftSlaveMotor2.follow(leftMasterMotor);
+    rightSlaveMotor1.follow(rightMasterMotor);
+    rightSlaveMotor2.follow(rightMasterMotor);
+
+    leftMasterEncoder = new CANEncoder(leftMasterMotor);
+    leftSlaveEncoder1 = new CANEncoder(leftSlaveMotor1);
+    leftSlaveEncoder2 = new CANEncoder(leftSlaveMotor2);
+
+    rightMasterEncoder = new CANEncoder(rightMasterMotor);
+    rightSlaveEncoder1 = new CANEncoder(rightSlaveMotor1);
+    rightSlaveEncoder2 = new CANEncoder(rightSlaveMotor2);
+
+    leftMasterEncoder.setPositionConversionFactor(DISTANCE_PER_REVOLUTION);
+    leftSlaveEncoder1.setPositionConversionFactor(DISTANCE_PER_REVOLUTION);
+    leftSlaveEncoder2.setPositionConversionFactor(DISTANCE_PER_REVOLUTION);
+
+    rightMasterEncoder.setPositionConversionFactor(DISTANCE_PER_REVOLUTION);
+    rightSlaveEncoder1.setPositionConversionFactor(DISTANCE_PER_REVOLUTION);
+    rightSlaveEncoder2.setPositionConversionFactor(DISTANCE_PER_REVOLUTION);
+
+    leftMasterEncoder.setVelocityConversionFactor(DISTANCE_PER_REVOLUTION / SECONDS_PER_MINUTE);
+    leftSlaveEncoder1.setVelocityConversionFactor(DISTANCE_PER_REVOLUTION / SECONDS_PER_MINUTE);
+    leftSlaveEncoder2.setVelocityConversionFactor(DISTANCE_PER_REVOLUTION / SECONDS_PER_MINUTE);
+
+    rightMasterEncoder.setVelocityConversionFactor(DISTANCE_PER_REVOLUTION / SECONDS_PER_MINUTE);
+    rightSlaveEncoder1.setVelocityConversionFactor(DISTANCE_PER_REVOLUTION / SECONDS_PER_MINUTE);
+    rightSlaveEncoder2.setVelocityConversionFactor(DISTANCE_PER_REVOLUTION / SECONDS_PER_MINUTE);
+
+    gyro = new AHRS(SPI.Port.kMXP);
+    
+    odometry = new DifferentialDriveOdometry(new Rotation2d(0));
+  }
+
+  /**
+   * 
+   * @param throttle throttle (foward positive)
+   * @param twist twist (clockwise positive)
+   * @param squareInputs square inputs
+   */
+  public void arcadeDrive(double throttle, double twist, boolean squareInputs) {
+    if (squareInputs) {
+      throttle = Math.copySign(throttle * throttle, throttle);
+      twist = Math.copySign(twist * twist, twist);
+    }
+
+    throttle = throttle < THROTTLE_DEADBAND ? 0 : throttle;
+    twist = twist < TWIST_DEADBAND ? 0 : twist;
+
+    double leftOutput = throttle + twist;
+    double rightOutput = throttle - twist;
+
+    leftOutput = leftOutput > MAX_OUTPUT ? MAX_OUTPUT : leftOutput;
+    rightOutput = rightOutput > MAX_OUTPUT ? MAX_OUTPUT : rightOutput;
+
+    leftMasterMotor.set(leftOutput);
+    rightMasterMotor.set(rightOutput);
+  }
+
+  /**
+   * 
+   * @return total positon of left encoders in meters
+   */
+  public double getLeftEncoders() {
+    return (leftMasterEncoder.getPosition() 
+          + leftSlaveEncoder1.getPosition()
+          + leftSlaveEncoder2.getPosition())/3;
+  }
+
+  /**
+   * 
+   * @return total position of right encoders in meters
+   */
+  public double getRightEncoders() {
+    return (rightMasterEncoder.getPosition()
+          + rightSlaveEncoder1.getPosition()
+          + rightSlaveEncoder2.getPosition())/3;
+  }
+
+  public Rotation2d getGyroAngle() {
+    return new Rotation2d(Math.toRadians(gyro.getAngle()));
+  }
+
+  public Pose2d getPosition() { 
+    return odometry.getPoseMeters();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    odometry.update(getGyroAngle(), getLeftEncoders(), getRightEncoders());
   }
 }
